e0a89508ba59f9c754391e7582bb73e7b8c63916
LiGaoYang <582355443@qq.com>
2020-01-18 14:22:51 +0800

    修改书架分组

--- src/api/request.js
+++ src/api/request.js
@@ -18,7 +18,7 @@ service.interceptors.request.use(
     config => {
         const token = getToken();
         if (token) {
-            config.headers['Authorization'] = 'Bearer ' + token;
+            config.headers['Authorization'] =  token;
         }
         return config
     },
