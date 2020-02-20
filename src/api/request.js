3efcf64f6aedc76ad55b8448036f7707ec9ea88d
LiGaoYang <582355443@qq.com>
2020-02-21 00:47:02 +0800

    添加Bearer字段

--- src/api/request.js
+++ src/api/request.js
@@ -18,7 +18,7 @@ service.interceptors.request.use(
     config => {
         const token = getToken();
         if (token) {
-            config.headers['Authorization'] =  token;
+            config.headers['Authorization'] = 'Bearer ' + token;
         }
         return config
     },
