7b5c320ebe00e5e1b29dd7863be1e4c3d9ce1bdc
LiGaoYang <582355443@qq.com>
2020-03-20 23:52:15 +0800

    打包

--- vue.config.js
+++ vue.config.js
@@ -150,7 +150,13 @@ module.exports = {
           })
         ]
       };
-
+      
+      // if (process.env.NODE_ENV === 'production') {
+      //   config.optimization.minimizer[0].options.terserOptions.compress.warnings = false
+      //   config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+      //   config.optimization.minimizer[0].options.terserOptions.compress.drop_debugger = true
+      //   config.optimization.minimizer[0].options.terserOptions.compress.pure_funcs = ['console.log']
+      // }
       // 生成gzip压缩文件
       //   const productionGzipExtensions = ['html', 'js', 'css']
       //   config.plugins.push(
